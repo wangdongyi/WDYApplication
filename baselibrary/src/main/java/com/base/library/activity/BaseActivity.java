79bf0ebabfe0c26b68a48aeed3ebb85ad3becf13
wangdongyi <wangdongyi@sunfield.mobi>
2017-06-19 15:21:17 +0800

    修改主题

--- baselibrary/src/main/java/com/base/library/activity/BaseActivity.java
+++ baselibrary/src/main/java/com/base/library/activity/BaseActivity.java
@@ -462,7 +462,6 @@ public class BaseActivity extends WDYActivity implements GestureDetector.OnGestu
         }
     }
 
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
